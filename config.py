e07678c5dcc181f77b1fa45678404981cc64c997
SaikoReyes <72665381+SaikoReyes@users.noreply.github.com>
2024-04-27 18:07:46 -0600

    Cambio de BD

--- config.py
+++ config.py
@@ -3,9 +3,9 @@ class Config:
     SECRET_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6OSwiZXhwIjoxNzEzODE3MzQ3fQ.GmHu8yBhJvmqNjQrHETBblpJX8lwAN7KXFOP0sg4XzA"
 
     #DB CONFIG
-    MYSQL_HOST = 'localhost'
-    MYSQL_USER = 'root'
-    MYSQL_PASSWORD = 'password'
+    MYSQL_HOST = 'melod-ia.mysql.database.azure.com'
+    MYSQL_USER = 'melodia'
+    MYSQL_PASSWORD = 'Password123!'
     MYSQL_DB = 'melodiadb'
 
     #DEBUG
